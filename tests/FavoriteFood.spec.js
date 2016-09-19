ab39080caa0674f23fd5e20b809e005fade0cb8f
Jason <jasonkwong11@gmail.com>
2016-09-18 21:39:34 -0400

    done

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,22 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should filter favorite food correctly', function(){
+    var mockedList =[{
+      name: "Lebron",
+      favoriteFood: "Pizza"
+    },{
+      name: "Jason",
+      favoriteFood: "Lobster"
+    },{
+      name: "Hippo",
+      favoriteFood: "Marbles"
+    }];
+
+    var results = $filter('favoriteFood')(mockedList, 'Lobster');
+
+    expect(results.length).toBe(1);
+    expect(results[0].name).toBe("Jason");
+  })
 	
 });
